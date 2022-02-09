ab92e72adb48a7b173e7a553474f3661f3563fc6
MAHMOUD-ELIMBABI <mahmoudembaby311@gmail.com>
2022-02-09 16:39:59 +0200

    assignment_2_Generic_GpioDriver

---
+++ assignement_2_Generic_GpioDriver/GPIO_ARM.c
@@ -0,0 +1,14 @@
+#include "GPIO_ARM.h"
+
+extern void ARM_pinMode(int pinNum , int pinMode)
+{
+
+}
+extern void ARM_digitalWrite(int pinNum , int pinMode)
+{
+
+}
+extern int ARM_digitalRead(int pinNum)
+{
+    return 0;
+}
---
+++ assignement_2_Generic_GpioDriver/GPIO_AVR.h
@@ -0,0 +1,9 @@
+#ifndef _GPIO_AVR_H_
+#define _GPIO_AVR_H_
+
+
+extern void AVR_pinMode(int pinNum , int pinMode);
+extern void AVR_digitalWrite(int pinNum , int pinMode);
+extern void AVR_digitalRead(int pinNum);
+
+#endif
